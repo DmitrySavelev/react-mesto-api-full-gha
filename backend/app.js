3f40bb2db098cc26fb4032c6b461dab6b0581f8e
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-04-10 01:17:51 +0700

    made server f + b

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const { PORT = 3000 } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 140, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,17 +1,19 @@
 const options = {
   origin: [
+    'http://mesto.savelev.nomoredomains.monster',
+    'https://mesto.savelev.nomoredomains.monster',
+    'http://api.mesto.savelev.nomoredomains.monster',
+    'https://api.mesto.savelev.nomoredomains.monster',
     'http://localhost:3000',
     'http://localhost:3001',
     'https://localhost:3000',
     'https://localhost:3001',
-    // 'https://ВАШ ДОМЕЙН С ДОКУМЕНТА',
-    // 'https://YOUR.github.io',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  // credentials: true,
+  credentials: false,
 };
 
 module.exports = options;
